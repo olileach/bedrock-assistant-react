880810167c5312a0bfad3034ad57e6428c2630ff
Oli Leach <oliverle@amazon.co.uk>
2024-09-02 15:01:20 +0100

    Added constants file to standardise text descriptions

--- frontend/src/Main/Constants/index.js
+++ frontend/src/Main/Constants/index.js
@@ -1,2 +1,8 @@
 export const TRANSCRIPTION_IN_PROGRESS = "Transcription in progress. Please wait.";
-export const BEDROCK_QUESTION_IN_PROGRESS = "Please wait while Bedrock answers your question";
+export const BEDROCK_QUESTION_IN_PROGRESS = "Please wait while Bedrock answers your question.";
+export const CONFIGURE_DEFAULT_MODEL = "Chose a model or use the default configured.";
+export const CONFIGURE_SUMMARY_MODEL = "Optional - Chose a different model to get a different summary output.";
+export const BEDROCK_FOLLOW_UP_WAIT = "\n\n\nPlease wait while your follow up question is answered....";
+export const RECORDING_NOT_TRANSCRIBED = "Recording not transcribed. Use the icon in the table to transcribe your recording.";
+export const TRANSCRIBE_OFF = "Off - Transcribe later using the transcribe button in the table below";
+export const TRANSCRIBE_ON = "On - Transcribe after the recording is saved";
